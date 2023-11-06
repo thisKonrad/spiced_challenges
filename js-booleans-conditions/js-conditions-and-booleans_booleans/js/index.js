f1d18bb4b09d96f3f461c996d08885dfae7d1e89
this.Konrad <konradlebt@gmail.com>
2023-11-06 15:27:32 +0100

    solved

--- js-booleans-conditions/js-conditions-and-booleans_booleans/js/index.js
+++ js-booleans-conditions/js-conditions-and-booleans_booleans/js/index.js
@@ -23,3 +23,18 @@ console.log(Boolean,"true : ", message);
 
 number = 0;
 console.log(Boolean,"0: ", number);
+
+number = 50;
+console.log(Boolean,"50: ", number);
+
+number = -50;
+console.log(Boolean,"-50: ", number);
+
+number = 5 > 1;
+console.log(Boolean,"5 > 1: ", number);
+
+number = 1 > 10;
+console.log(Boolean,"1 > 10: ", number);
+
+message = false;
+console.log(Boolean,"false: ", message);
