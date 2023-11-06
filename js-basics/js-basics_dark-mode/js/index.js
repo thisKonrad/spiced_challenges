b5e602b5b3debfeb7c2f0d1aa5e533234c5b0469
this.Konrad <konradlebt@gmail.com>
2023-11-06 12:07:11 +0100

    add buttons to js

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,7 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const lightBtn = document.querySelector('[data-js="light-button"]');
+const darkBtn = document.querySelector('[data-js="dark-button"]');
+const toggleBtn = document.querySelector('[data-js="toggle-button"]');
