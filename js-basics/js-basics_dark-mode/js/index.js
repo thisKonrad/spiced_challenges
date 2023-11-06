89a885a9997561acebbc0c71b0b03dabd84e6e05
this.Konrad <konradlebt@gmail.com>
2023-11-06 12:11:02 +0100

    add event listeners

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -2,6 +2,17 @@ console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
 
-const lightBtn = document.querySelector('[data-js="light-button"]');
-const darkBtn = document.querySelector('[data-js="dark-button"]');
+const lightBtn = document.querySelector('[data-js="light-mode-button"]');
+const darkBtn = document.querySelector('[data-js="dark-mode-button"]');
 const toggleBtn = document.querySelector('[data-js="toggle-button"]');
+
+
+darkBtn.addEventListener('click',()=>{
+    bodyElement.classList.add('dark');
+});
+lightBtn.addEventListener('click',()=>{
+    bodyElement.classList.remove('dark');
+});
+toggleBtn.addEventListener('click',()=>{
+    bodyElement.classList.toggle('dark');
+});
