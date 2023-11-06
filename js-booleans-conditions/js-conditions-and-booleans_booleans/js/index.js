3c6828e67f18cc895324c5b7e405e665c4597c64
this.Konrad <konradlebt@gmail.com>
2023-11-06 15:25:16 +0100

    add first boolean console.logs

--- js-booleans-conditions/js-conditions-and-booleans_booleans/js/index.js
+++ js-booleans-conditions/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,25 @@
 console.clear();
 
 console.log(Boolean("Try it out"));
+
+
+let number = 2 == "2";
+console.log(Boolean("2==2: ", number));
+
+number = 2 === "2";
+console.log(Boolean("2==='2': ", number));
+
+number = 2 === 2;
+console.log(Boolean("2===2: ", number));
+
+let message = "hello World";
+console.log(Boolean, message);
+
+message = "";
+console.log(Boolean," '': ", message);
+
+message = true;
+console.log(Boolean,"true : ", message);
+
+number = 0;
+console.log(Boolean,"0: ", number);
