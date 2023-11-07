9166ce410ea1305405275f868fb38458bb35929e
this.Konrad <konradlebt@gmail.com>
2023-11-07 16:51:24 +0100

    solved pizza display

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -17,6 +17,7 @@ pizzaInput1.addEventListener("input", () => {
   pizzaSize1 = pizzaInput1.value;
 
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1,pizzaSize1);
 
 });
 console.log("size1: ", pizzaSize1);
@@ -26,6 +27,7 @@ pizzaInput2.addEventListener("input", () => {
   pizzaSize2 = pizzaInput2.value;
 
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2,pizzaSize2);
 
 });
  
@@ -53,13 +55,20 @@ function calculatePizzaGain(diameter1, diameter2){
 
 
   output.innerText = `${pizzaAreaRelation}`;
-  console.log("Relation:_ ", pizzaAreaRelation);
 
 };
-//calculatePizzaGain(400,500)
+
 
 // Task 2
 // define the function updatePizzaDisplay here
+
+function updatePizzaDisplay(pizzaElement,newSize){
+
+  let newDisplaySize = newSize / 24 * 100;
+
+  pizzaElement.style.width = `${newDisplaySize}px`;
+
+};
 
 // Task 3
 // define the function updateOutputColor here
