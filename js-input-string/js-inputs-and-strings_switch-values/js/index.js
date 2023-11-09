7e82fd51abf027212e20866e6ebbd814e2bdf30d
this.Konrad <konradlebt@gmail.com>
2023-11-09 11:01:59 +0100

    solved

--- js-input-string/js-inputs-and-strings_switch-values/js/index.js
+++ js-input-string/js-inputs-and-strings_switch-values/js/index.js
@@ -6,7 +6,8 @@ Switch the input values of the two input fields when clicking the button
 - [ ] Assign the second input element to a variable
 - [ ] Add a third button (a switch button) to the index.html
 - [ ] Assign the third button to a variable
-- [ ] Make sure that when the switch button is clicked, the values of the two input fields are being switched
+- [ ] Make sure that when the switch button is clicked, 
+the values of the two input fields are being switched
 - [ ] Make sure that all three buttons work on both input fields
 
 Hint: the HTML entity for the switch button is: &updownarrow
@@ -25,5 +26,11 @@ uppercaseButton.addEventListener("click", () => {
 });
 
 lowercaseButton.addEventListener("click", () => {
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+
+switchButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
