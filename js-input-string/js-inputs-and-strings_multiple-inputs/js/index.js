70ad94361bdfc22171a129c4a42ed0a0826d95a2
this.Konrad <konradlebt@gmail.com>
2023-11-09 11:18:05 +0100

    dont understand the goal of this challenge but i solved it soi far as i understood the meaning

--- js-input-string/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-input-string/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -3,9 +3,11 @@
 Important: Create a fork for each sub challenge!
 Implement the following functionality:
 
-1. On button click: The value of the first input field is copied into the second input field
+1. On button click: The value of the first 
+input field is copied into the second input field
 
-2. On button click: The value of the first input field is copied in uppercase into the second input field
+2. On button click: The value of the first 
+input field is copied in uppercase into the second input field
 
 3. On button click: The values of the two input fields switch.
 
@@ -14,3 +16,12 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener('click',()=>{
+
+    secondInput.value = firstInput.value.toUpperCase();
+
+    firstInput.value = secondInput.value;
+
+
+});
