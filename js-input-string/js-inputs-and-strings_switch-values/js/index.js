932debf03ecb98d81478ed000f92ed09505b5237
this.Konrad <konradlebt@gmail.com>
2023-11-09 10:56:50 +0100

    addd switch btn

--- js-input-string/js-inputs-and-strings_switch-values/js/index.js
+++ js-input-string/js-inputs-and-strings_switch-values/js/index.js
@@ -18,6 +18,7 @@ const secondInput = document.querySelector('[data-js="second-input"]');
 
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
