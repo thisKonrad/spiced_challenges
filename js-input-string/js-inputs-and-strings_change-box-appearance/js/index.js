eb71a06243fd2dbbfc3f6b01681e057f97f23f55
this.Konrad <konradlebt@gmail.com>
2023-11-09 11:41:58 +0100

    solved task one

--- js-input-string/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-input-string/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,13 +1,22 @@
 /*
 
-Change the appearance of the box according to the values of the sliders (color, border radius, rotation)
+Change the appearance of the box according to the values of 
+the sliders (color, border radius, rotation)
 
 – [ ] Add an event listener for each input element.
-– [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
-– [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
-– [ ] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
+– [ ] When the slider control of inputColor is moved, 
+change the background color of the box. Hint: You need to work with the 
+hsl() functional notation for colors.
 
-Hint: You can work with the property "style", just like you worked with the property "classList".
+– [ ] When the slider control of inputRadius is moved, 
+change the border radius of the box. If the control reaches the right end, 
+make the box look like a circle.
+– [ ] When the slider control of inputRotation is moved, 
+rotate the box. Hint: You may need to work with the CSS property 
+"transform" and a value that contains "rotate" and "deg".
+
+Hint: You can work with the property "style", 
+just like you worked with the property "classList".
 
 For further information check MDN:
 - about inputs of type="range": https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
@@ -16,3 +25,23 @@ For further information check MDN:
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
 */
+const box = document.querySelector('.box');
+
+const colorRange = document.querySelector('[data-js="input-color"]');
+const radius = document.querySelector('[data-js="input-radius"]');
+const rotation = document.querySelector('[data-js="input-rotation"]');
+
+
+colorRange.addEventListener('input',()=>{
+
+    let rangeValue = colorRange.value;
+
+        console.log(rangeValue)
+        //let randomHSL = Math.floor(Math.random()*100);
+        //console.log(randomHSL);
+        box.style.backgroundColor = `hsl(${rangeValue}, 70%, 80%)`;
+
+})
+
+
+
