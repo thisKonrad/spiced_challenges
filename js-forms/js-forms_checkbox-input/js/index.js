1cf5596abdf0c51cda88611d6de089bb50aaf178
this.Konrad <konradlebt@gmail.com>
2023-11-10 11:24:32 +0100

    created toggle functionality for error message

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -4,21 +4,46 @@ const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
+
+tosCheckbox.addEventListener('change',()=>{
+  hideTosError()
+});
+
 function hideTosError() {
-  tosError.setAttribute("hidden", "");
-}
 
-function showTosError() {
-  tosError.removeAttribute("hidden");
-}
+  if(tosCheckbox.checked) {
+    tosError.style.visibility = "hidden";
+  }
+  else {
+    tosError.style.visibility = "visible";
+  }
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+};
 
+
+
+/* function showTosError() {
+
+  if(tosCheckbox.checked = true) {
+    //tosError.removeAttribute("hidden",false);
+    tosError.setAttribute("hidden", true);
+  }
+
+}; */
+
+
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
   // --v-- write your code here --v--
 
-  // --^-- write your code here --^--
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
 
-  // eslint-disable-next-line no-alert
   alert("Form submitted");
+
+  e.target.reset();
+
 });
