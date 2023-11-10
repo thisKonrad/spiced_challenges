8f2b89fa14afd3a8c2dc2658c5da824b211d9c7d
this.Konrad <konradlebt@gmail.com>
2023-11-10 13:13:39 +0100

    add a and b number from dom

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -2,31 +2,29 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
+const numA = documnet.querySelector('#number-a').value;
+const numB = documnet.querySelector('#number-b').value;
 
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
 
 form.addEventListener("submit", (event) => {
+
   event.preventDefault();
 
   let result;
 
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
+  result = add(numA,numB),subtract(numA,numB),multiply(numA,numB),divide(numA,numB);
 
   resultOutput.textContent = result;
 });
