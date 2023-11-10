3a30503d63abd4ed0e14d190d50d987c6555ca94
this.Konrad <konradlebt@gmail.com>
2023-11-10 15:43:12 +0100

    solved

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,17 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+
+
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newMessage = document.createElement("li");
+  newMessage.textContent = "Im a New Message!";
+  newMessage.classList.add('toast-container__message');
+  toastContainer.appendChild(newMessage);
 });
 
-clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+clearButton.addEventListener("click", (e) => {
+  //Exercise: Clear the stack of toast messages
+toastContainer.innerHTML = "";
 });
