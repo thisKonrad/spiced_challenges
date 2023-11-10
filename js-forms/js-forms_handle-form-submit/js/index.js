c3d77743d224fce4047f14be1e2e7b7613f48923
this.Konrad <konradlebt@gmail.com>
2023-11-10 10:58:58 +0100

    solved badness bonus task

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,6 +1,9 @@
 console.clear();
 
+
 const form = document.querySelector('[data-js="form"]');
+const age = document.querySelector('#age').value;
+const badness = document.querySelector('#badness').value;
 const firstName = document.querySelector('#first-name');
 
 form.addEventListener('submit', (e)=>{
@@ -10,8 +13,15 @@ form.addEventListener('submit', (e)=>{
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
     console.log(data);
-    
+
+    let ageBadness = age + badness;
+
+    let message = `The age-badness-sum of "${firstName.value}" is "${ageBadness}"`;
+
+    console.log(message);
+
     e.target.reset();
     firstName.focus();
-    
+
 });
+
