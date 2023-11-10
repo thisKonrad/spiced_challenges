4a62fd5d02a4c9b880ae7f248f8cbaf9c941916c
this.Konrad <konradlebt@gmail.com>
2023-11-10 10:47:28 +0100

    add focus to firstname

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,14 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const firstName = document.querySelector('#first-name');
 
 form.addEventListener('submit', (e)=>{
+
     e.preventDefault();
 
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
-
     console.log(data);
     
     e.target.reset();
+    firstName.focus();
+    
 });
