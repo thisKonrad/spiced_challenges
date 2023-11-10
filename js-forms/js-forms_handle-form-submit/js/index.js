82ddad4e3696ab6ee7df82469bd01be5f5d96e3d
this.Konrad <konradlebt@gmail.com>
2023-11-10 10:43:55 +0100

    add basic formdata insatnace

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+    
+    e.target.reset();
+});
