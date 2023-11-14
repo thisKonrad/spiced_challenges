45a7d9851a2b75af703d216560776a267c4aeddf
this.Konrad <konradlebt@gmail.com>
2023-11-14 17:35:57 +0100

    add the stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,12 +2,18 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(starcount = 0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+for( let i = 1; i < 6; i++){
+  
+  const newStar = document.createElement('img');
+  newStar.src= './assets/star-empty.svg';
+  starContainer.append(newStar);
 
+}
   //--^-- your code here --^--
 }
 
