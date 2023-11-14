83ded0a123ae29374b610b67042b85946898ef5f
this.Konrad <konradlebt@gmail.com>
2023-11-14 17:49:17 +0100

    solved so far.. but not perfect

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,16 +2,24 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars(starcount = 0) {
+function renderStars() {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-for( let i = 1; i < 6; i++){
-  
-  const newStar = document.createElement('img');
-  newStar.src= './assets/star-empty.svg';
-  starContainer.append(newStar);
+  for( let i = 1; i < 8; i++){
+
+    const newStar = document.createElement('img');
+    newStar.src= './assets/star-empty.svg';
+
+    newStar.addEventListener('click',(e)=>{
+
+
+      newStar.src= './assets/star-filled.svg';
+
+    })
+
+    starContainer.append(newStar);
 
 }
   //--^-- your code here --^--
