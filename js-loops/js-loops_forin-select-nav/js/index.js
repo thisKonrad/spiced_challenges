22e8ce147dade17b4f3877b1889398d7f1a830bd
this.Konrad <konradlebt@gmail.com>
2023-11-14 16:00:23 +0100

    unsolved

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -35,15 +35,18 @@ for(const opt in languages){
   select.append(optionElem);
 
 };
-
-
-
 // --^-- write/change code here --^--
 
 
 
 // Part 2: Creating a Navigation Bar
 
+
+
+
+
+
+// --v-- write/change code here --v--
 const nav = {
   home: { href: "/home", text: "Home" },
   about: { href: "/about", text: "About" },
@@ -52,9 +55,24 @@ const nav = {
 
 const navElement = document.createElement("nav");
 const ul = document.createElement("ul");
-main.append(navElement);
 navElement.append(ul);
+main.append(navElement);
 
-// --v-- write/change code here --v--
 
+for( const key in nav){
+
+  console.log("n: ", nav[key].href);
+
+  const hrefNav = nav[key].href;
+  const anchorTag = document.createElement("a");
+  anchorTag.setAttribute("href", hrefNav);
+
+  const liItem = document.createElement("li");
+  const textNav = nav[key].text;
+  liItem.setAttribute("text", textNav);
+
+
+  ul.append(anchorTag)
+  ul.append(liItem)
+};
 // --^-- write/change code here --^--
