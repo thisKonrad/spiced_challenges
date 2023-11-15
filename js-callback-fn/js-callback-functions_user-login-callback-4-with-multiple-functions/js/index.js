7784593af5580b14fef0d3371b46048dc8b951ab
this.Konrad <konradlebt@gmail.com>
2023-11-15 11:17:37 +0100

    solved

--- js-callback-fn/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js-callback-fn/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -8,4 +8,22 @@ function handleUserLogin(onSuccess, onError, userName) {
   }
 }
 
+
 // The exercise starts here!
+
+
+function showWelcomeMessage(userName,userRole){  
+  console.log(`Welcome ${userName}! 
+  You are logged in now as ${userRole}`)
+};
+
+
+function showErrorMessage(errorMessage){
+  console.log(`Login error: ${errorMessage}`);
+}
+
+console.log("function howMessage: -----------------------")
+handleUserLogin(showWelcomeMessage,showErrorMessage, 'Jane Doe');
+
+console.log("function error -----------------------")
+handleUserLogin(showWelcomeMessage,showErrorMessage, 'John Doe');
