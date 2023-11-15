3ec53636be656e4ef4fa3521bea76a081b8392a8
this.Konrad <konradlebt@gmail.com>
2023-11-15 09:09:08 +0100

    pull main

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,26 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars = 0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for( let i = 1; i < 6; i++){
 
+    const newStar = document.createElement('img');
+
+    if(i <= filledStars){
+      newStar.src= './assets/star-filled.svg';
+    }else{
+      newStar.src= './assets/star-empty.svg';
+    }
+
+    newStar.addEventListener('click',renderStars(i));
+
+    starContainer.append(newStar);
+
+  }
   //--^-- your code here --^--
 }
-
-renderStars();
+renderStars(3);
