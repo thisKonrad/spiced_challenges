0795ab53d2c4a2863229bd24627dc25ec41501f6
this.Konrad <konradlebt@gmail.com>
2023-11-15 10:23:43 +0100

    solved so far

--- js-callback-fn/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-fn/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -1,8 +1,20 @@
 console.clear();
 
 function handleUserLogin(onSuccess) {
-  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
-  onSuccess();
+  // You might wonder why this function is called `onSuccess`. 
+  //This will become clear by the fourth challenge! 🤞
+  onSuccess()
 }
 
 // Call handleUserLogin below!
+console.log('Funtion call (function) 01: ----------------');
+
+handleUserLogin(showWelcomeMessage);
+function showWelcomeMessage(){
+  console.log('Welcome! You are logged in now.')
+};
+
+console.log('Funtion call (arrow) 02: ----------------');
+
+handleUserLogin(()=>{console.log('Welcome! You are logged in now.')});
+
