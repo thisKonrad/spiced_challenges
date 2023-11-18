1eb6c2a5db77716745add1bd1bb627ed47439511
this.Konrad <konradlebt@gmail.com>
2023-11-18 13:42:14 +0100

    n

--- js-array-methods-002/js-array-methods-2_news-flash/index.js
+++ js-array-methods-002/js-array-methods-2_news-flash/index.js
@@ -4,20 +4,24 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 
 const container = document.querySelector('[data-js="card-container"]');
 
+
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
+const filteredNews = news.filter((news) => {
+  news.categories.includes("politics")
 });
+console.log("filtered News: ", filteredNews)
+
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.sort((a,b)=> a.body.length -= b.body.length);
+console.log("sorted News: ",sortedNews)
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
   container.append(cardElement);
 });
 
+
 // Check your filter and sorting order here:
 checkFilteredNews(filteredNews);
-
 checkSortedNews(sortedNews);
