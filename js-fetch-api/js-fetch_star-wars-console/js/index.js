901fb6854119fc4cdd8baac6db2b8266f417edca
this.Konrad <konradlebt@gmail.com>
2023-11-21 11:00:17 +0100

    get eye color

--- js-fetch-api/js-fetch_star-wars-console/js/index.js
+++ js-fetch-api/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,27 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
 
+    try{
+        const getStarWars = await fetch("https://swapi.dev/api/people");
+
+        console.log("Star Wars API: ",getStarWars)
+
+        const data = await getStarWars.json();
+
+        console.log("Data. ",data)
+
+        const r2D2EyeColor = data.results[1].eye_color;
+
+        console.log("R2D2: ", r2D2EyeColor)
+    }
+    catch (error){
+        
+        console.error(error);
+    }
+}
 fetchData();
+
+
+
