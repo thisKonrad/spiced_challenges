dd5e2c4bd5b4783e7d2b234727e2fc9038f2400d
this.Konrad <konradlebt@gmail.com>
2023-11-22 11:08:05 +0100

    test faileds or id ont get it.-

--- js-unit-test/js-unit-testing_get-animal/index.js
+++ js-unit-test/js-unit-testing_get-animal/index.js
@@ -1,1 +1,16 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+
+    if(animal === 'cat'){
+        return 'I totally love cats!';
+    }
+    if(animal === typeof 'string' && animal !== 'cat'){
+        const animalPluralName = animal + "s";
+        return `I like [${animalPluralName}]!`;
+    }
+    if(!animal){
+        return 'I do not like animals at all!';
+    }
+
+}
+getAnimal();
+
