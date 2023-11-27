9bc0950d008aabf409e817860948a205de920dfe
this.Konrad <konradlebt@gmail.com>
2023-11-27 10:58:13 +0100

    solved so far

---
+++ react-basics/react-basics_hello-world/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  //return <h1>Hello World!</h1>;
+  return <h1>HelloWorld</h1>
+}
+
+
+function HelloWorld(){
+ return <h1>Hello World!</h1>;
+}
