b64ae07b569dbdf1fb06ceb9e5bc5fbb20939c4a
this.Konrad <konradlebt@gmail.com>
2023-11-29 13:26:33 +0100

    solved far

--- react-state/react-state_emoji-checker/src/App.js
+++ react-state/react-state_emoji-checker/src/App.js
@@ -3,7 +3,6 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
-  //let code = "?";
 
   const validCode = "🐡🐠🐋";
 
@@ -11,25 +10,16 @@ export default function App() {
 
   function handleClick(e){
     console.log(e.target.textContent)
-    console.log("click")
     setEmoji(emoji + e.target.textContent)
   }
   function resetClick(){
-    console.log("click")
-
-    if(emoji === validCode){
-      setEmoji("Valid Code!") 
-    }
-    else {
-      setEmoji(" ")
-    }
-   
+    setEmoji(" ")
   }
 
 
   return (
     <div className="container">
-      <h1>current code : <span id="code">{emoji}</span> </h1>
+      <h1>current code : {emoji}</h1>
       <div className="button-container">
 
         <button type="button"
@@ -57,7 +47,8 @@ export default function App() {
       <button type="button" onClick={resetClick}>
         Reset
       </button>
- {/*     {emoji === validCode && <p>Valid code!</p>} */}
+
+   {emoji === validCode && <p>Valid code!</p>} 
     </div>
   );
 }
