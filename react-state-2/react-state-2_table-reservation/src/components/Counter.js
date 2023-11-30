f0599c712ffa48ecc639a7eec1873f38247fb729
this.Konrad <konradlebt@gmail.com>
2023-11-30 12:15:53 +0100

    people__table

---
+++ react-state-2/react-state-2_table-reservation/src/components/Counter.js
@@ -0,0 +1,26 @@
+
+
+export default function Counter({onPeopleUp, onPeopleDown}) {
+
+  return (
+    <>
+      <h2>How many people would you like to visit us with?</h2>
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment people count"
+          onClick={onPeopleUp}>
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement people count"
+          onClick={onPeopleDown}>
+          -
+        </button>
+      </div>
+    </>
+  );
+}
