b76d4ff706847f0a8736915c23ebc9eff68150e6
this.Konrad <konradlebt@gmail.com>
2023-12-06 16:16:47 +0100

    added all pages

--- next-js-basic/lotr-app/pages/index.js
+++ next-js-basic/lotr-app/pages/index.js
@@ -1,7 +1,45 @@
+import Link from "next/link";
+
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>The Lord Of The Rings App</h1>
+      <ul>
+        <li>
+          <Link href="/volumes">Volumes</Link>
+       </li>
+      </ul>
     </div>
   );
 }
+
+
+
+/* 
+export default function HomePage() {
+  return (
+    <div>
+      <h1>The Lord Of The Rings</h1>
+      <ul>
+      <li>
+        <Link href="/"></Link>
+      </li>
+      <li>
+        <Link href="/volumes">Volumes</Link>
+      </li>
+      <li>
+        <Link href="/volumes/the-fellowship-of-the-ring">Volumes The Fellowship Of The Ring</Link>
+      </li>
+      <li>
+        <Link href="/volumes/the-two-towers">Volumes The Two Towers</Link>
+      </li>
+      <li>
+        <Link href="/volumes/the-return-of-the-king">Volumes The Return Of The King</Link>
+      </li>
+    </ul>
+    </div>
+  );
+}
+ */
+
---
+++ next-js-basic/lotr-app/pages/volumes.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { introduction } from "libary/libary.js";
+
+
+export default function Volumes() {
+    return (
+      <div>
+        <h1>The Lord Of The Rings</h1>
+        <article>
+            <p>{introduction}</p>
+        </article>
+        <h2>All Volumes:</h2>
+        <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/volumes">Volumes</Link>
+        </li>
+        <ul>
+            <li>
+            <Link href="/volumes/the-fellowship-of-the-ring">Volumes The Fellowship Of The Ring</Link>
+            </li>
+            <li>
+            <Link href="/volumes/the-two-towers">Volumes The Two Towers</Link>
+            </li>
+            <li>
+            <Link href="/volumes/the-return-of-the-king">Volumes The Return Of The King</Link>
+            </li>
+        </ul>
+      </ul>
+      </div>
+    );
+  }
+  
